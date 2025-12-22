3036eae845f27186e9dfb06e62fe7ed38e794f0a
axuel5044 <d2006machardy@gmail.com>
2025-12-21 22:48:27 -0500

    Week 1: Variables and Output

---
+++ week1/variables.py
@@ -0,0 +1,7 @@
+myName = str("Darnell")
+myAge = int(19)
+myFavoriteGame = str("ArcaneOdyssey")
+
+print(myName, "is my name.", f"I am {myAge}." , (myFavoriteGame[0:6]), (myFavoriteGame[6:13]), "is my favorite game.")
+
+#print("Testing: I am", 35, "years old.")
