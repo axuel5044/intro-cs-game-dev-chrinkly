8c7119e54d1ca86ea04fb34191162bd4420b7f5a
axuel5044 <d2006machardy@gmail.com>
2025-12-21 23:43:32 -0500

    Week 1: User Input and Conditionals.

---
+++ week1/input_conditionals.py
@@ -0,0 +1,16 @@
+print("What is your name?")
+
+name = input("Name:")
+
+print("How old are you, cutie?")
+
+age = int(input("Age:"))
+
+if age < int(13):
+ print("You're too young for me...")
+if age > int(13) and age < int(17):
+ print("Just don't get too freaky.")
+if age > int(17):
+ print("Tryna grab some Taco Bell at 3 AM?")
+
+ # No bugs as far as I know
